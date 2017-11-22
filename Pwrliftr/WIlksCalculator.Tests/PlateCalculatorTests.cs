72f68111146968699cfb117a576fb5bad81c9ac0
Aviio <connorwilson1@googlemail.com>
2017-11-22 22:08:49 +0000

    Adds more tests, cleans up plate calculator

--- Pwrliftr/WIlksCalculator.Tests/PlateCalculatorTests.cs
+++ Pwrliftr/WIlksCalculator.Tests/PlateCalculatorTests.cs
@@ -25,7 +25,7 @@ namespace Pwrliftr.Calculators.Tests
 
 
         [TestCase]
-        public void TestRecordWeight()
+        public void TestRecordWeight_332half()
         {
             //arrange
             var sut = new PlateCalculator();
@@ -34,6 +34,51 @@ namespace Pwrliftr.Calculators.Tests
             //act
             var result = sut.Calculate(plateSet, 435, true);
             var expected = new List<double>() { 25, 25, 25, 25, 25, 25, 20, 15, 10, 5, 2.5, 1.25, 0.5, 0.25 };
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase]
+        public void Test332Set()
+        {
+            //arrange
+            var sut = new PlateCalculator();
+            //actually half a plate set
+            var plateSet = new List<double>() { 25, 25, 25, 25, 20, 15, 10, 5, 2.5, 1.25 };
+
+            //act
+            var result = sut.Calculate(plateSet, 332.5, false);
+            var expected = new List<double>() { 25, 25, 25, 25, 20, 15, 10, 5, 2.5, 1.25 };
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase]
+        public void Test334Set()
+        {
+            //arrange
+            var sut = new PlateCalculator();
+            //actually half a plate set
+            var plateSet = new List<double>() { 25, 25, 25, 25, 20, 15, 10, 5, 2.5, 1.25, 0.5, 0.25 };
+
+            //act
+            var result = sut.Calculate(plateSet, 334, true);
+            var expected = new List<double>() { 25, 25, 25, 25, 20, 15, 10, 5, 2.5, 1.25, 0.5, 0.25 };
+            //assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase]
+        public void Test682half()
+        {
+            //arrange
+            var sut = new PlateCalculator();
+            //actually half a plate set
+            var plateSet = new List<double>() { 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 15, 10, 5, 2.5, 1.25};
+
+            //act
+            var result = sut.Calculate(plateSet, 592.5, true);
+            var expected = new List<double>() { 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 15, 10, 5, 2.5, 1.25 };
             //assert
             Assert.AreEqual(expected, result);
         }
--- Pwrliftr/WilksCalculator/PlateCalculator.cs
+++ Pwrliftr/WilksCalculator/PlateCalculator.cs
@@ -8,48 +8,32 @@ namespace Pwrliftr.Calculators
         public List<double> Calculate(List<double>plateSet, double targetWeight, bool isRecordAttempt)
         {
             //change this to array
-            List<double> plates = isRecordAttempt ? new List<double>() { 25, 20, 15, 10, 5, 2.5, 1.25, 0.5, 0.25 } : new List<double>() { 25, 20, 15, 10, 5, 2.5, 1.25 };
-
+            List<double> weights = isRecordAttempt ? new List<double>() { 25, 20, 15, 10, 5, 2.5, 1.25, 0.5, 0.25 } : new List<double>() { 25, 20, 15, 10, 5, 2.5, 1.25 };
             var result = new List<double>() { };
-            var requestedWeight = targetWeight;
-            
+            //minus bar and collar weight, divide by 2 as we want to get one half of the plate weight
             targetWeight = (targetWeight - 25) / 2;
-
-            foreach (var plate in plates)
+            foreach (var weight in weights)
             {
-                targetWeight = targetWeight / plate;
+                targetWeight = targetWeight / weight;
                 var rounded = Math.Floor((double)targetWeight);
                 double remainder = 0;
                 if (rounded > 0)
                 {
                     for (int i = 1; i <= rounded; i++)
                     {
-                        if (!plateSet.Contains(plate))
+                        if (!plateSet.Contains(weight))
                         {
-                            remainder = (rounded - i) * plate;
+                            remainder = (rounded - i) * weight;
                             break;
                         }
-                        result.Add(plate);
-                        plateSet.Remove(plate);
+                        result.Add(weight);
+                        plateSet.Remove(weight);
                     }
                 }
-              
-                //get remainder
                 remainder = remainder + (targetWeight - rounded);
-               
-                //get remainder
-                targetWeight = plate * remainder;
+                targetWeight = weight * remainder;
             }
-
             return result;
         }
-    }
-
-    public class PlateSet
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-
-        public double[] Plates { get; set; }
     }
 }
